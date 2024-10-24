c1ec68863c72a11142edb8a53cd837f6ff3a103f
Code-n-Canvas <helloiamnobody1@gmail.com>
2024-10-24 22:49:55 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import LocomotiveScroll from "locomotive-scroll";
+
+  let locomotiveScroll: any;
+
+  onMount(() => {
+    locomotiveScroll = new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]") as HTMLElement,
+      smooth: true,
+    });
+
+    return () => {
+      if (locomotiveScroll) {
+        locomotiveScroll.destroy();
+      }
+    };
+  });
+</script>
+
+<div data-scroll-container>
+  <section>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-red-500">Hello world!</h1>
+  </section>
+</div>
