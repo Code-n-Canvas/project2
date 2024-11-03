0e34f4088c0fd4bd3dada52d4e59090669f17a95
Code-n-Canvas <helloiamnobody1@gmail.com>
2024-11-03 21:39:35 +0800

    "Updated Header.svelte and +page.svelte with changes to layout, styling, and content."

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,15 +19,28 @@
 </script>
 
 <div data-scroll-container>
-  <section class="flex flex-col items-center justify-center h-screen" style="height: 80vh; margin-top: -20px;">
-    <!-- Red Box -->
-    <div class="relative shadow-2xl grid place-items-center justify-center -mt-10">
-      <h1 class="text-8xl font-dynapuff text-white">Cozy Creatives</h1>
+  <section
+    class="flex flex-col items-center justify-center h-screen"
+    style="height: 80vh; margin-top: -20px;"
+  >
+    <div
+      class="relative shadow-2xl grid place-items-center justify-center -mt-10"
+    >
+      <h1 class="text-8xl max-sm:text-4xl font-dynapuff text-white">
+        Cozy Creatives
+      </h1>
     </div>
 
-    <!-- Yellow Box -->
-    <!-- <div class="border-4 border-yellow-500 w-1/3 h-20 mt-10 flex items-center justify-center">
-      <button class="bg-blue-500 text-white px-4 py-2 rounded">Show</button>
-    </div> -->
+    <hr class="h-px w-[80%] mx-auto my-8 bg-white border-2" />
+
+    <div class="w-[80%]">
+      <p class="text-center text-lg">
+        Cozy Creatives is your go-to online shop for unique,
+        digitally-illustrated merchandise that brings charm and creativity to
+        everyday items. Check out our collection of witty stickers, whimsical
+        bookmarks, and playful keychains—each piece designed to add a cozy,
+        creative touch to your life.
+      </p>
+    </div>
   </section>
 </div>
